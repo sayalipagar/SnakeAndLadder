aba8e4c4da28028ea3b7bbc83e5e580aa1ace325
sayalipagar <pagarsayali565@gmail.com>
2022-08-24 16:57:56 +0530

    [sayali]Add:Welcome to SnakeAndLadderProgram

---
+++ SnakeandLadder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeandLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Problem Statement!");
+        }
+    }
+}
